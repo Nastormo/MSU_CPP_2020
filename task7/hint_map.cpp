e09bf290078fa10461efc4eed1aab8fa89707d34
Lev <lyovasmirnov@gmail.com>
2020-12-21 22:15:29 +0300

    Add chrono

--- task7/hint_map.cpp
+++ task7/hint_map.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <map>
 #include <iomanip>
+#include <chrono>
 
 int main(int argc, char const *argv[])
 {
     int num_iter = 1e7;
     std::string str = "MAP";
-    std::map<int, std::string> hint_map;
-    auto [it, success] = hint_map.insert({0, str + "0"});
+    std::map<int, int> hint_map;
+    auto start = std::chrono::system_clock::now();
+    auto [it, success] = hint_map.insert({0, 0});
     for (int i = 1; i < num_iter; i++) {
-        it = hint_map.insert(it, {i, str + std::to_string(i)});
+        it = hint_map.insert(it, {i, i});
     }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time insert: " << elapsed.count() << "s" << std::endl;
 
-    int count = 0;
-    for (int i = 0; i < num_iter; i++) {
-        std::string check = hint_map[i];
-        if (check != str + std::to_string(i)) count++;
-    }
-    std::cout << count;
 	return 0;
 }
--- task7/map.cpp
+++ task7/map.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <map>
 #include <iomanip>
+#include <chrono>
 
 int main(int argc, char const *argv[])
 {
     int num_iter = 1e7;
     std::string str = "MAP";
-    std::map<int, std::string> map;    
-    map.insert({0, str + "0"});
+    std::map<int, int> map; 
+    auto start = std::chrono::system_clock::now();   
+    map.insert({0, 0});
     for (int i = 1; i < num_iter; i++) {
-        map.insert({i, str + std::to_string(i)});
+        map.insert({i, i});
     }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time insert: " << elapsed.count() << "s" << std::endl;
 
-    int count = 0;
-    for (int i = 0; i < num_iter; i++) {
-        std::string check = map[i];
-        if (check != str + std::to_string(i)) count++;
-    }
-    std::cout << count;
 	return 0;
 }
