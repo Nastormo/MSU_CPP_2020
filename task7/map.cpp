a738f60b11ebf8a7a1e3432170e8bff8bf6794ef
Lev <lyovasmirnov@gmail.com>
2020-12-21 23:58:49 +0300

    Add rand order

--- task7/map.cpp
+++ task7/map.cpp
@@ -3,19 +3,88 @@
 #include <iomanip>
 #include <chrono>
 
+std::map<int, int> hint_order_number(int num_number) {
+    std::map<int, int> map;
+    for (int i = 0; i < num_number; i++) {
+        map.insert(map.end(), {i, i});
+    }
+    return map;
+}
+
+std::map<int, int> order_number(int num_number) {
+    std::map<int, int> map;  
+    for (int i = 0; i < num_number; i++) {
+        map.insert({i, i});
+    }
+    return map;
+}
+
+std::map<double, double> rand_number(int num_number) {
+    std::map<double, double> map; 
+    double num;
+    for (int i = 0; i < num_number; i++) {
+        num = rand() / RAND_MAX - 0.5 * i;
+        map.insert({num, num});
+    }
+    return map;
+}
+
+std::map<double, double> hint_rand_number(int num_number) {
+    std::map<double, double> map; 
+    double num;
+    double nmax, nmin = 0; 
+    for (int i = 0; i < num_number; i++) {
+        num = rand() / RAND_MAX - 0.5 * i;
+        if (num > nmax) {
+            nmax = num;
+            map.insert(map.end(), {num, num});
+        } else if (num < nmin) {
+            nmin = num;
+            map.insert(map.begin(), {num, num});
+        } else {
+            map.insert({num, num});
+        }
+    }
+    return map;
+}
+
 int main(int argc, char const *argv[])
 {
-    int num_iter = 1e7;
-    std::string str = "MAP";
-    std::map<int, int> map; 
-    auto start = std::chrono::system_clock::now();   
-    map.insert({0, 0});
-    for (int i = 1; i < num_iter; i++) {
-        map.insert({i, i});
+    int num_number = 1e4;
+    int num_iter = 1e3;
+
+    auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < num_iter; i++) {
+        order_number(num_number);
     }
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Elapsed time insert: " << elapsed.count() << "s" << std::endl;
+    std::cout << "Elapsed time order number: " << elapsed.count() << "s" << std::endl;
 
+    start = std::chrono::system_clock::now();
+    for (int i = 0; i < num_iter; i++) {
+        hint_order_number(num_number);
+    }
+    end = std::chrono::system_clock::now();
+    elapsed = end - start;
+    std::cout << "Elapsed time hint order number: " << elapsed.count() << "s" << std::endl;
+
+    srand(0);
+    start = std::chrono::system_clock::now();
+    for (int i = 0; i < num_iter; i++) {
+        rand_number(num_number);
+    }
+    end = std::chrono::system_clock::now();
+    elapsed = end - start;
+    std::cout << "Elapsed time rand order number: " << elapsed.count() << "s" << std::endl;
+
+    srand(0);
+    start = std::chrono::system_clock::now();
+    for (int i = 0; i < num_iter; i++) {
+        hint_rand_number(num_number);
+    }
+    end = std::chrono::system_clock::now();
+    elapsed = end - start;
+    std::cout << "Elapsed time rand hint order number: " << elapsed.count() << "s" << std::endl;
 	return 0;
 }
--- task7/run.sh
+++ task7/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-g++ map.cpp -o map
-g++ -std=c++1z hint_map.cpp -o hint_map
-
-echo "map"
-time ./map
-echo "hint map"
-time ./hint_map
+g++ -std=c++1z map.cpp -o map
+./map
 echo "Done"
